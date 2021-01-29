3580e32a7f7b46cd3efe325bdfca54a2d9163baa
salim9 <salimaniatv@gmail.com>
2021-01-29 21:09:28 +0300

    first commit

---
+++ ProjectLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by Salim Uzun on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
